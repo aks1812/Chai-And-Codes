cf6c9a7593484c4eda0c84cbc654750cb8c11d07
Ashish Shrivastava <ashishkk.2020@gmail.com>
2023-11-28 09:44:42 +0000

    #main

---
+++ Basic_Js/01_Variable.js
@@ -0,0 +1,36 @@
+const accountId = 144553
+let accountEmail = "Ashish@google.com"
+var accountPassword = "1234560"
+acountCity = "Jaipur"
+
+/*
+Prefer not to use "var"
+Because of issue in block scope & functional scope
+*/
+
+
+// accountId = 2 // not allowed
+ accountEmail = "ashish@uptechinfo.com"
+ accountPassword = "1212121"
+ acountCity = "Delhi"
+ let acountStaate;
+ let Ankit;
+
+console.log(accountId);
+
+
+
+console.table([accountId , accountEmail , accountPassword, acountCity, acountState]);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
