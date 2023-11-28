dd7675eee9fc2469678835dff67d22a6c98e3ca9
Ashish Shrivastava <ashishkk.2020@gmail.com>
2023-11-28 09:46:34 +0000

    #02_DataType.js

---
+++ Basic_Js/02_DataType.js
@@ -0,0 +1,17 @@
+let name = "Ashish";
+let age = 18;
+let isloggedIn = false;
+
+
+
+//Number => 2  to power 53;
+// bigint 
+//String => true / False
+// Null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object 
+
+console.log(typeof age);
